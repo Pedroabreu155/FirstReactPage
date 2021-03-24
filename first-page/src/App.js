fdd7762e1367423afbfa9488422f960cce7be67e
Pedro Abreu <pedroliveirabreu@gmail.com>
2021-03-24 09:45:52 -0300

    links added

--- first-page/src/App.js
+++ first-page/src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Seja bem vindo a Minha Página!</h1>
+        <a target={'blank'} href={'https://www.instagram.com/_abreus_/'}>Instagram</a>
+        <a target={'blank'} href={'https://github.com/Pedroabreu155'}>Github</a>
+        <a target={'blank'} href={'https://www.linkedin.com/in/pedro-de-abreu-oliveira-99a6491a7/'}>Linkedin</a>
       </header>
     </div>
   );
